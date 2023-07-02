6e5c6a3aaa158aa8e7e32cd9e97426419ffb455f
Sandeep <sandeepkumarrana49@gmailcom>
2023-07-02 12:31:04 +0530

    first commit

---
+++ Frontend/src/Components/Signup.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+
+function Signup() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSignup = (e) => {
+    e.preventDefault();
+    // Handle signup logic here (e.g., API call to the backend)
+    // You can access the signup data (name, email, phoneNumber, password) here
+  };
+
+  return (
+    <div>
+      <h2>Signup Page</h2>
+      <form onSubmit={handleSignup}>
+        <label>
+          Name:
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Email:
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Phone Number:
+          <input
+            type="text"
+            value={phoneNumber}
+            onChange={(e) => setPhoneNumber(e.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <br />
+        <button type="submit">Signup</button>
+      </form>
+    </div>
+  );
+}
+
+export default Signup;
